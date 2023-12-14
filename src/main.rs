25ca86f1d39680e37e797f68ed13b4b26126fa94
Gambhir Rathore <grathore0110@gmail.com>
2023-12-14 17:07:13 +0530

    Add some thoeritical concept and conventions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    // main function is the entry point into the program.
+
+    // `Statement` are instructions that perform some action and do not return a value.
+    // `Expressions` evaluate to a resultant value. Let’s look at some examples.
+
+    // Rust is an expression-based language.
+
+    /*
+        Conventions : 
+        - Rust code uses snake case as the conventional style for function and variable names.
+        - Naming convention for constants is to use all uppercase with underscore between words
+    */ 
+
     println!("Hello, world!");
 }
